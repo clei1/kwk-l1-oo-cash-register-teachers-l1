11d1f8470ec92f855b3c3876212f118199030de6
clei1 <callmecon328@gmail.com>
2018-07-21 21:45:57 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -4,7 +4,9 @@ class AmazonTransaction
   attr_accesor :items, :discount, :total, :last_transaction
 
   def initialize(d = 0)
-    
+    @total = 0
+    @discount = discount
+    @items = []
   end
 
 
