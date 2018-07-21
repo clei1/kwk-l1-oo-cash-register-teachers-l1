021876cb6d546435c3b17cbac9fc3b4167b145a8
clei1 <callmecon328@gmail.com>
2018-07-21 21:50:25 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -17,6 +17,7 @@ class AmazonTransaction
   def apply_discount
     if(@discount == 0)
       return "There is no discount to apply."
+    end
     @total *= (1- @discount/100.0)
     return "After the discount, the total comes to $#{@total.to_i}."
   end
